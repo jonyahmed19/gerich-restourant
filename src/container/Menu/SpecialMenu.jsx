644a8bbf275f20b37202d4c14780470ce50c216e
Sahidul Islam <jonyahmed19@gmail.com>
2022-02-07 19:44:07 +0600

    completed till special menu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
-
-import './SpecialMenu.css';
+import React from "react";
+import data from "../../constants/data";
+import { SubHeading, MenuItem } from "../../components";
+import "./SpecialMenu.css";
+import images from "../../constants/images";
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className="app__specialMenu flex__center section__padding">
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu that fits your power" />
+      <h1 className="headtext__cormorant">Today's special</h1>
+    </div>
+    <div className="app__specialMenu-menu">
+      <div className="app__specialMenu-menu_wine flex__center">
+        <p className="app__specialMenu-menu_heading">Wine & Beer</p>
+        <div className="app_specialMenu_menu_items">
+          {data?.wines?.map((wine, index) => (
+            <MenuItem
+              key={wine.title + index}
+              title={wine.title}
+              price={wine.price}
+              tags={wine.tags}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="app__specialMenu-menu_img">
+        <img src={images.menu} alt="Menu img" />
+      </div>
+      <div className="app__specialMenu-menu_cocktails flex__center">
+        <p className="app__specialMenu-menu_heading">Cocktails</p>
+        <div className="app_specialMenu_menu_items">
+          {data?.cocktails?.map((cocktail, index) => (
+            <MenuItem
+              key={cocktail.title + index}
+              title={cocktail.title}
+              price={cocktail.price}
+              tags={cocktail.tags}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+    <div style={{ marginTop: "15px" }}>
+      <button type="button" className="custom__button">
+        View More
+      </button>
+    </div>
   </div>
 );
 
