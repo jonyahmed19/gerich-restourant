24c9450ede591505692b58b032525ad5d0e05ae5
Sahidul Islam <jonyahmed19@gmail.com>
2022-02-07 07:09:22 +0600

    completed the header part

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React from "react";
+import { SubHeading } from "../../components";
+import images from "../../constants/images";
 
-import './Header.css';
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Find Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        It is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout. The point of
+        using Lorem Ipsum is that it has a more-or-less normal distribution of
+        letters, as opposed to using 'Content here, content here', making it
+        look like readable English. Many desktop publishing packages and web
+        page editors now use Lorem Ipsum as their default model text, and a
+        search for 'lorem ipsum' will uncover many web sites
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="Welcome" />
+    </div>
   </div>
 );
 
